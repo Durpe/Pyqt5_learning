8f2f351440147a8c04b33f0070c78cda415f2e89
Durpe <47975576+Durpe@users.noreply.github.com>
2020-12-02 22:54:49 +0800

    QT练习

---
+++ install.py
@@ -0,0 +1,45 @@
+import json
+from PySide2.QtWidgets import QApplication,QMainWindow,QPushButton,QPlainTextEdit
+from PySide2.QtCore import QFile
+from PySide2.QtUiTools import QUiLoader
+import requests
+class Stats:
+
+    def __init__(self):
+        # 调用ui文件
+        qfile = QFile('ui/stats.ui')
+        qfile.open(QFile.ReadOnly)
+        qfile.close()
+
+        self.ui = QUiLoader().load(qfile)
+        self.ui.pushButton.clicked.connect(self.greetings)
+
+
+    def greetings(self):
+        #r = requests.get('http://www.4399.com')
+        self.ui.plainTextEdit_2.clear()
+        try:
+            r = requests.get(self.ui.lineEdit.text())
+            r.encoding = 'GBK'
+            txt = r.headers
+            txt = dict(txt)
+            for k,v in txt.items():
+                self.ui.plainTextEdit_2.insertPlainText(k)
+                self.ui.plainTextEdit_2.insertPlainText(":" + v + "\n")
+                #self.ui.plainTextEdit_2.insertPlainText('\n')
+        except Exception as e:
+            self.ui.plainTextEdit_2.clear()
+            self.ui.plainTextEdit_2.insertPlainText(str(e))
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    window = Stats()
+    window.ui.show()
+
+    app.exec_()
